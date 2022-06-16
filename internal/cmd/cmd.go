6ceb5629d10a38511a3fb4bd741f234aa44d9f9c
jimmy <475772164@qq.com>
2022-06-16 20:16:21 +0800

    移除初始化Hello接口

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -6,8 +6,6 @@ import (
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/net/ghttp"
 	"github.com/gogf/gf/v2/os/gcmd"
-
-	"GaAdmin/internal/controller"
 )
 
 var (
@@ -19,9 +17,7 @@ var (
 			s := g.Server()
 			s.Group("/", func(group *ghttp.RouterGroup) {
 				group.Middleware(ghttp.MiddlewareHandlerResponse)
-				group.Bind(
-					controller.Hello,
-				)
+				group.Bind()
 			})
 			s.Run()
 			return nil
